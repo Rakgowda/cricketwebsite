b7884de38993f654132225ef9a322dbd063d58f3
Rakgowda <rakshithgowdakv@gmail.com>
2019-09-26 21:45:21 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+
+//  AOS.init();
+const vi = document.querySelector("h1")
+
+const sec = document.querySelector(".vi")
+const dd = document.querySelector(".carimage")
+const logo = document.querySelector(".image")
+const ul = document.querySelector("ul")
+
+
+
+
+var t1 = new TimelineMax();
+
+
+// t1.fromTo(logo,.5,{opacity:"0"},{opacity:"1",ease : Bounce.easeOut})
+t1.fromTo(logo,1,{height:"0px"},{height:"150px",ease : Bounce.easeOut})
+// t1.fromTo(ul,.3,{opacity:"0"},{opacity:"1",ease : Power2.easeInOut})
+
+
+t1.fromTo(dd,1,{height:"0px"},{height:"400px",ease : Power2.easeInOut})
+
+
+
+t1.fromTo(vi,.5,{height:"0%"},{height:"10%"})
+
+t1.fromTo(sec,.5,{opacity:"0"},{opacity:"1"})
+// t1.fromTo(sec,1,{width:"0%"},{width:"100%"})
+
+// t1.fromTo(sec,.5,{height:"0%"},{height:"50%",ease : Power2.easeInOut})
+
+
+$(document).ready(function() {
+    
+    
+    var s = $(".author_image");
+					   
+	$(window).scroll(function() {
+		var windowpos = $(window).scrollTop();
+		if (windowpos >500) {
+            s.addClass("r");
+           
+		} else {
+			s.removeClass("r");	
+		}
+	});
+});
